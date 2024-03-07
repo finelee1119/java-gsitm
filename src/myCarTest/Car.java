87b02e57c1c50878d0a44e19f152c7e8ec9f9a4c
FINE <finelee1119@gmail.com>
2024-03-07 15:29:47 +0900

    commit change

--- src/myCarTest/Car.java
+++ src/myCarTest/Car.java
@@ -2,24 +2,28 @@ package myCarTest;
 
 public class Car {
     // 멤버변수
-    final String brand = "Hyundai";
-    final String carName = "Sonata";
+    private String brand;
+    private String carName;
     private String color;
     private int speed = 0;
 
     // 메서드
-    void SetColor(String color) {
+    void setCarInfo(String brand, String carName, String color) {
+        this.brand = brand;
+        this.carName = carName;
         this.color = color;
+        System.out.println("브랜드명은 " + this.brand + " 입니다.");
+        System.out.println("차종은 " + this.carName + " 입니다.");
         System.out.println("색상은 " + this.color + " 입니다.");
     }
 
-    void setCurrentSpeed(int speed) {
-        this.speed = speed;
-        System.out.println("현재 속도는 " + this.speed + "km 입니다.");
+    void currentSpeed(int currentSpeed) {
+        this.speed = currentSpeed;
+        System.out.println("현재 속도는 " + currentSpeed + "km 입니다.");
     }
 
-    void speedUp(int speed) {
-        this.speed += speed;
+    void speedUp(int increaseSpeed) {
+         this.speed += increaseSpeed;
         if (this.speed >= 100) {
             System.out.println("현재 속도는 " + this.speed + "km 입니다.");
             System.out.println("과속입니다.");
@@ -28,8 +32,8 @@ public class Car {
         }
     }
 
-    void stop(int speed) {
-        this.speed -= speed;
+    void stop(int decreaseSpeed) {
+        this.speed -= decreaseSpeed;
         if (this.speed > 0) {
             System.out.println("현재 속도는 " + this.speed + "km 입니다.");
         } else {
--- src/myCarTest/MyCar.java
+++ src/myCarTest/MyCar.java
@@ -4,21 +4,16 @@ public class MyCar {
     public static void main(String[] args) {
       // Car를 생성합니다.
         Car car = new Car();
-        System.out.println("차량을 구입했습니다.");
-        System.out.println("브랜드명은 " + car.brand + " 입니다.");
-        System.out.println("차종은 " + car.carName + " 입니다.");
-
-      // 색상 설정
-        car.SetColor("White");
+        car.setCarInfo("Hyundai", "Sonata", "White");
 
       // 속도 설정 및 가속 (10가속)
         System.out.println("------------------------");
-        car.setCurrentSpeed(50);;
+        car.currentSpeed(50);;
         car.speedUp(10);
 
       // 속도 설정 및 감속 (멈춰보기)
         System.out.println("------------------------");
-        car.setCurrentSpeed(40);;
+        car.currentSpeed(40);;
         car.stop(10);
         car.stop(10);
         car.stop(10);
@@ -26,7 +21,7 @@ public class MyCar {
 
       // 속도 설정 및 가속 (과속)
         System.out.println("------------------------");
-        car.setCurrentSpeed(0);;
+        car.currentSpeed(0);;
         car.speedUp(10);
         car.speedUp(10);
         car.speedUp(10);
