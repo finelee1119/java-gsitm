53ec6c108074f8d3fe3c7d479152d4c8de9d4b36
FINE <finelee1119@gmail.com>
2024-03-07 15:33:53 +0900

    commit change

--- src/myCarTest/MyCar.java
+++ src/myCarTest/MyCar.java
@@ -14,24 +14,17 @@ public class MyCar {
       // 속도 설정 및 감속 (멈춰보기)
         System.out.println("------------------------");
         car.currentSpeed(40);;
-        car.stop(10);
-        car.stop(10);
-        car.stop(10);
-        car.stop(10);
+
+        for (int i = 0; i < 4; i++) {
+            car.stop(10);
+        }
 
       // 속도 설정 및 가속 (과속)
         System.out.println("------------------------");
         car.currentSpeed(0);;
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
-        car.speedUp(10);
+
+        for (int i = 0; i < 11; i++) {
+            car.speedUp(10);
+        }
     }
 }
